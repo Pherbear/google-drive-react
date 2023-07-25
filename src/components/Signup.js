ecbe1a0d48176c6ce4889f013e9eabec181ec4a6
Bryan <bryan.garcia.cuevas@gmail.com>
2023-07-24 23:27:03 -0700

    able to now register new users

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,11 +1,33 @@
-import React, { useRef } from 'react'
-import {Form, Button, Card} from 'react-bootstrap'
+import React, { useRef, useState } from 'react'
+import {Form, Button, Card, Alert} from 'react-bootstrap'
+import { useAuth } from '../contexts/AuthContext'
 
 export default function Signup() {
 
     const emailRef = useRef()
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
+    const { signup, currentUser } = useAuth()
+    const [error, setError] = useState('')
+    const [loading, setLoading] = useState(false)
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+
+        if (passwordRef.current.value !==
+            passwordConfirmRef.current.value){
+            return setError('Passwords do not match')
+        }
+        try {
+            setError('')
+            setLoading(true)
+            await signup(emailRef.current.value, passwordRef.current.value)
+        } catch {
+            setError('Failed to create an account') 
+        }
+
+        setLoading(false)
+    }
 
   return (
     <>
@@ -14,7 +36,9 @@ export default function Signup() {
                 <h2 className="text-center mb-4">
                     Sign Up
                 </h2>
-                <Form>
+                {currentUser && currentUser.email}
+                {error && <Alert variant = "danger">{error}</Alert>}
+                <Form onSubmit= {handleSubmit}>
                     <Form.Group id="email">
                         <Form.Label>
                             Email
