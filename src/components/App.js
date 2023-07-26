790c91905181531d9072843870faae183b1604d1
Bryan <bryan.garcia.cuevas@gmail.com>
2023-07-26 01:28:10 -0700

    added dashboard and login pages

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,9 @@ import React from 'react'
 import Signup from './Signup';
 import { Container } from 'react-bootstrap'
 import { AuthProvider } from '../contexts/AuthContext';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from "./Dashboard"
+import Login from "./Login"
 
 function App() {
   return (
@@ -11,7 +14,15 @@ function App() {
         style={{minHeight: "100vh"}}
       >
         <div className='w-100' style={{ maxWidth: '400px'}}>
-          <Signup />
+          <Router>
+            <AuthProvider>
+              <Routes>
+                <Route exact path= "/" Component={Dashboard} />
+                <Route path="/signup" Component={Signup} />
+                <Route path="/login" Component={Login} />
+              </Routes>
+            </AuthProvider>
+          </Router>
         </div>
       </Container>
     </AuthProvider> 
